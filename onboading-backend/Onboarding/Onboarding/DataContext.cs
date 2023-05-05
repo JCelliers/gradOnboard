8bd5a860d46a967ae9adc6fab124d2a0e16594b1
JCelliers <123621270+JCelliers@users.noreply.github.com>
2023-05-05 18:31:13 +0200

    Init

---
+++ onboading-backend/Onboarding/Onboarding/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+public class DataContext : DbContext
+{
+    public DataContext() { }
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddJsonFile("appsettings.json");
+        var configuration = builder.Build();
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+    }
+}
